ca4bc1db8a3b37fed8bce91f4c24ece3f38277ca
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-29 22:18:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,7 +199,7 @@ def big_shoe_rebounds
   game_hash.each do |team_name, team_info|
     team_info[:players].each do |player|
       players[:player_name].each do |stats|
-        
+
           binding.pry
         end
     end
