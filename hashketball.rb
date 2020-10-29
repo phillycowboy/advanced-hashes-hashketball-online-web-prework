360c084c35a47fc0ecdcc572352a55aa8df03c2e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-29 19:34:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,7 +177,7 @@ def player_numbers(number)
           new_jersey_array.push(team_info[:number])
 
             # binding.pry
-          # end
+          end
       end
     end
   new_jersey_array
