75802444eabf007bc709bb44823fea98caa9123d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-29 19:55:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,7 +172,7 @@ end
 def player_numbers(number)
   new_jersey_array=[]
     game_hash.each do |team_name, team_info|
-      team_info[:players].each do |number|
+      team_info[:players].each do |player|
          team_info[:players] == [:number]
           new_jersey_array.push(player[:number])
 
