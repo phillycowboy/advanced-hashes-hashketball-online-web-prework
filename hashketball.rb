57fb3b063ec03a9d03f3a0d2a5c03ea3bf3b6008
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-29 17:03:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,7 +156,7 @@ def team_colors(name)
         team_info.each do |team, colors|
           binding.pry
         if  name == colors
-          # binding.pry
+          binding.pry
           return  team_info[:colors]
       end
     end
