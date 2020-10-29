9c20fac56918a181c123a614af832d2a5fcb107d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-29 17:42:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -164,7 +164,7 @@ end
 def team_names
 game_hash.each do |teams, team_info|
     team_info.each do |home, away|
-      if team_name==team_name
+      if [:team_name]==[:team_name]
         return team_info[:team_name]
         binding.pry
       end
